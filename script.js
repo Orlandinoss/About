8d6c800262ef56ca9c8bcc930c41b27d4b09e863
Orlandinoss <lockutudor@gmail.com>
2025-02-24 14:05:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function greet(name, lastName){
+    console.log("hello " + name + " " + lastName);
+}
+
+function square(num){
+    return num * num;}
+
+
+let numberr = square(200);
+console.log(numberr)
